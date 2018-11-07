83fa8dfb0115794aeaf4edf294837731972e62fc
Baoza-J <guantangbaoza@163.com>
2018-11-07 20:15:03 +0800

    /home/jerry/Spider/venv/lib/python3.6/site-packages/bs4/element.py:1110: UserWarning: .nonExistingTag is deprecated, use .find("nonExisting") instead. If you really were looking for a tag called nonExistingTag, use .find("nonExistingTag")   name=tag_name

--- 01_chapter_初见网络爬虫/scrapetest.py
+++ 01_chapter_初见网络爬虫/scrapetest.py
@@ -1,5 +1,20 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), features='html.parser')
-print(bsObj.h1)
+try:
+    html = urlopen("http://pythonscraping.com/pages/page1.html")
+    if html is None:
+        print('url is not found')
+except HTTPError as e:
+    print(e)
+else:
+    bsObj = BeautifulSoup(html.read(), features='html.parser')
+    try:
+        badContent = bsObj.find('nonExistingTag').anotherTag
+    except AttributeError as e:
+        print('Tag was not found')
+    else:
+        if badContent == None:
+            print('Tag was not found')
+        else:
+            print(badContent)
+#    print(bsObj.nonExistentTag.someTag)
